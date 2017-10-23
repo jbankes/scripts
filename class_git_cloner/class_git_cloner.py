a2afcdc88ba61dde11b84b49cff12c693203351f
Justin Bankes <jbankes@mail.csuchico.edu>
2017-10-22 17:18:10 -0700

    Added class git cloner script

---
+++ class_git_cloner/class_git_cloner.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+#!/usr/local/bin/python3
+# File: class_git_cloner
+# Description: This script will clone the same repository from each student. The
+#   only thing that needs to happen is a students.txt file with all of the 
+#   GitHub account names listed on individual lines. 
+import sys, os
+from git import Repo
+
+# Open student file and read
+try: 
+    students_file = open('students.txt', 'r')
+except:
+    print('Students.txt is missing, please try again')
+    sys.exit()
+students = students_file.read().splitlines()
+
+# Read Repo name and create new directory
+repo_name = input('Repository Name for Assignment: ')
+os.mkdir(repo_name)
+
+# Loop through students and clone all repos
+for student in students:
+    git_url = 'https://github.com/' + student + '/' + repo_name
+    repo_dir = repo_name + '/' + student 
+    Repo.clone_from(git_url, repo_dir)
+
