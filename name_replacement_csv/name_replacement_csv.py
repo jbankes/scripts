7dac113080a4ab62e34ec80417ef4d5916a1e8e6
Justin Bankes <justin@liatrio.com>
2020-08-22 20:14:04 -0600

    feat: add writing to new file and check for preferred name

--- name_replacement_csv/name_replacement_csv.py
+++ name_replacement_csv/name_replacement_csv.py
@@ -3,6 +3,12 @@ import os
 import csv
 import argparse
 
+# preferred names two tuple
+preferred_names = {
+            ('Some', 'Person'): ('Some', 'Person'),
+            ('Some', 'Person'): ('Some', 'Person')
+        }
+
 # Read file name from commandline
 parser = argparse.ArgumentParser(description='Format names for transfer between programs.')
 parser.add_argument('filename', metavar='csv_filename', help='Filename to format')
@@ -17,18 +23,26 @@ print('-------------------------------------------------------------------')
 
 # Read CSV file and augment names
 with open(args.filename) as csv_file:
-    print(f'Reading {args.filename}')
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            split_name = row[0].split()
-            print(f'{row[0]} will save as \'{split_name[1]},{split_name[0]}\'')
-            line_count += 1
-    print(f'Read {line_count} lines.')
-
-#  with open(new_filename, 'w', newline='') as csv_file:
-#      csv_writer = csv.writer(new_filename, delimiter=',')
+    with open(new_filename, 'w', newline='') as new_csv_file:
+        csv_writer = csv.writer(new_csv_file, delimiter=',')
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                row.pop(0)
+                titles = ['lastname','firstname']
+                row = titles + row
+                csv_writer.writerow(row)
+                line_count += 1
+            else:
+                split_name = row[0].split()
+                first_last = row.pop(0)
+                name = first_last.split()
+                if (name[0], name[1]) in preferred_names:
+                    preferred_name = preferred_names[(name[0], name[1])]
+                    row = [preferred_name[1],preferred_name[0]] + row
+                else:
+                    row = [name[1],name[0]] + row
+                csv_writer.writerow(row)
+                line_count += 1
+        print(f'Adjust {line_count} lines.')
